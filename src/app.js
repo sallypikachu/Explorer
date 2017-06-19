a5990d2508f4b47a764d6dfdf10e62a4ae19a102
sallypikachu <sallypikachu@gmail.com>
2017-06-18 22:11:24 -0400

    Can POST users to ExplorerBackend

--- src/app.js
+++ src/app.js
@@ -1,67 +1,30 @@
-import React, { Component } from 'react';
-import { View, Button } from 'react-native';
-import { StackNavigator, addNavigationHelpers } from 'react-navigation';
+import React from 'react';
+import { Button } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 import LoginForm from './components/LoginForm';
 import SignUpScreen from './components/SignUpScreen';
-import { Provider, connect } from 'react-redux';
-
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-
-import thunk from 'redux-thunk';
 
 const Routes = {
   Login: {
     screen: LoginForm,
-    navigationOptions: () => ({
-      headerRight: <Button title='Sign Up' onPress={() => this.props.navigation.navigate('SignUp')} />
-    })
+    // navigationOptions: () => ({
+    //   headerRight: <Button
+    //     title='Sign Up'
+    //     onPress={() => this.props.navigation.navigate('SignUp')}
+    //   />
+    // })
   },
   SignUp: {
     screen: SignUpScreen,
-    navigationOptions: () => ({
-      headerRight: <Button title='Log In' />
-    })
+    // navigationOptions: () => ({
+    //   headerRight: <Button
+    //     title='Log In'
+    //     onPress={() => this.props.navigation.navigate('Login')}
+    //   />
+    // })
   }
 };
 
 const App = StackNavigator(Routes, { initialRouteName: 'Login' });
-const AppNavigator = StackNavigator(Routes, { initialRouteName: 'Login' });
-
-const initialState = AppNavigator.router.getStateForAction(AppNavigator.router.getActionForPathAndParams('Login'));
-
-const navReducer = (state, action) => {
-  const newState = AppNavigator.router.getStateForAction(action, state);
-  return newState || state;
-};
-
-function getRootReducer(navReducer) {
-  return combineReducers({
-      nav: navReducer
-  });
-}
-
-function getStore(navReducer) {
-  const store = createStore(
-    getRootReducer(navReducer),
-    undefined,
-    applyMiddleware(thunk)
-  );
-  return store;
-}
-
-class AppWithNavigationState extends Component {
-  render() {
-    return (
-      <AppNavigator
-        navigation={addNavigationHelpers({
-          dispatch: this.props.dispatch,
-          state: this.props.nav
-        })}
-      />
-    );
-  }
-}
-
-const store = getStore(navReducer);
 
 export default App;
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -6,24 +6,69 @@ class LoginForm extends Component {
     title: 'Log In'
  };
 
+ constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  async onLoginPress() {
+    const { email, password } = this.state;
+    console.log(email);
+    console.log(password);
+    try {
+      await fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            user: {
+                email: email,
+                password: password,
+                password_confirmation: password
+            }
+        })
+      })
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+        debugger;
+      });
+    } catch (errors) { console.log(errors); }
+  }
+
   render() {
     const { navigate } = this.props.navigation;
     return (
       <Card>
         <CardSection>
           <Input
+            secureTextEntry
             placeholder='user@email.com'
             label='Email'
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
           />
         </CardSection>
         <CardSection>
           <Input
+            secureTextEntry
             placeholder='password'
             label='Password'
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
           />
         </CardSection>
         <CardSection>
-          <MyButton>Submit</MyButton>
+          <MyButton
+            onPress={
+              this.onLoginPress.bind(this)
+            }
+          >Submit</MyButton>
         </CardSection>
         <CardSection>
           <MyButton
